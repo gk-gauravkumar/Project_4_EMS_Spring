5bb0f7983da2c22827214d5dcf392a289a48b5d3
gk-gauravkumar <gaurav@Gauravs-Mac-mini.local>
2023-05-07 14:55:42 +0530

    created logs for spring application

--- src/main/java/com/gaurav/RestApplication.java
+++ src/main/java/com/gaurav/RestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
 @SpringBootApplication
 @ComponentScan({"com.gaurav.controller","com.gaurav.services"})
 @EntityScan("com.gaurav.entity")
--- src/main/java/com/gaurav/controller/EmployeeController.java
+++ src/main/java/com/gaurav/controller/EmployeeController.java
@@ -2,6 +2,8 @@ package com.gaurav.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,11 +20,14 @@ import com.gaurav.services.EmployeeServices;
 @RequestMapping("/employee/")
 public class EmployeeController {
 	
+	Logger log = LoggerFactory.getLogger(EmployeeController.class);
+	
 	@Autowired
 	private EmployeeServices employeeServices;
 	
 	@GetMapping("getall")
 	public List<Employee> getAll(){
+		log.info("inside info of getAll method");
 		return employeeServices.getAll();
 	}
 	@PostMapping("insert")
